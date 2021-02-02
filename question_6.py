4202da374bb719e3c4b72f6d60369c7f6eb08f01
= <=>
2021-02-02 21:03:24 +0200

    renamed files

---
+++ question_6.py
@@ -0,0 +1,48 @@
+#import
+#simple implementatin using builtin methods
+def remove_space(str_1):
+    new_str = str_1.split()
+    new_str = " ".join(new_str)
+    return new_str
+
+#implementation using loops
+def remove_last_space(str_p, len_str):
+    new_str = ""
+    i = 0 
+    while (i < len_str - 1):
+        new_str += str_p[i]
+        i += 1
+    return new_str
+
+def remove_space2(str_1):
+    "remove all spaces"
+    if type(str_1) is not str:
+        #raise type error when the input is not a string
+        raise TypeError
+
+    new_str = ""
+    count = 0                   #count the number of white spaces
+    check_index = 0             #check if the space is at index 1
+    endHasSpace = False         #check if the str ends with a white space
+    for letter in str_1:
+        if letter == " ":
+            if (check_index == 0) and count == 0:
+                #check if the first index is a space
+                count += 1
+                pass
+            elif count == 0:
+                #not the first index is a space and the previous index is a letter
+                new_str += letter
+                count += 1
+                endHasSpace = True
+        else:
+            #normal condition
+            count = 0
+            new_str += letter
+            endHasSpace = False  
+        check_index += 1
+
+    if endHasSpace == True:
+        #remove last space
+        new_str = remove_last_space(new_str, len(new_str))
+    return new_str
---
+++ question_7_test.py
@@ -0,0 +1,20 @@
+#import
+from question_7 import *
+import unittest
+
+class Test_count_letters(unittest.TestCase):
+    #test the method count letters
+    def test_with_empty(self):
+        #pass en empty string
+        self.assertEqual(count_letters(""), {})
+    
+    def test_with_non_empty(self):
+        #pass a string
+        ex_dic = {"d": 1, "i": 1, "r": 1, "a":1, "c":1}
+        self.assertDictEqual(count_letters("dirac"), ex_dic)
+    
+    def test_type_error(self):
+        #passing other element that str
+        values = [True, 1, 1.5, {"d" : "2"}, ["abc", "def"], None]
+        for element in values:
+            self.assertRaises(TypeError, count_letters, element)
